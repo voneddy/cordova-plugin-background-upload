ddc1242291a35c0b01a2e75a7dcabdd4a5e99371
Eddy <johnmoran@sportplan.com>
2025-07-16 11:37:27 +0100

    Bump version to 7.0.0 and update various files for consistency and clarity

--- src/ios/FileTransferBackground.h
+++ src/ios/FileTransferBackground.h
@@ -2,10 +2,10 @@
 #import <Cordova/CDVPlugin.h>
 #import "FileUploader.h"
 
-@interface FileTransferBackground : CDVPlugin<FileUploaderDelegate>
--(void)startUpload:(CDVInvokedUrlCommand*)command;
--(void)removeUpload:(CDVInvokedUrlCommand*)command;
--(void)initManager:(CDVInvokedUrlCommand*)command;
--(void)acknowledgeEvent:(CDVInvokedUrlCommand*)command;
--(void)destroy:(CDVInvokedUrlCommand*)command;
+@interface FileTransferBackground : CDVPlugin <FileUploaderDelegate>
+- (void)startUpload:(CDVInvokedUrlCommand *)command;
+- (void)removeUpload:(CDVInvokedUrlCommand *)command;
+- (void)initManager:(CDVInvokedUrlCommand *)command;
+- (void)acknowledgeEvent:(CDVInvokedUrlCommand *)command;
+- (void)destroy:(CDVInvokedUrlCommand *)command;
 @end
